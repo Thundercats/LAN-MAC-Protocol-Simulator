a678bafcef0f728e12e5e444e2790aab3562ae62
Donald Thai <donald.thai@gmail.com>
2012-03-13 22:32:21 -0700

    printf

--- src/lmp/LMP.java
+++ src/lmp/LMP.java
@@ -12,12 +12,14 @@ public class LMP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    	/*
         ArrayList<Node> network = new ArrayList(); // Store the Nodes here
         
         for(int i = 0; i < NUM_OF_NODES; i++)
         {
            network.add(new Node()); //Add nodes into the ArrayList
            network.get(network.size()).transmit(); //Transmits the node we just created
-        }
+        }*/
+    	System.out.println("hello");
     }
 }
