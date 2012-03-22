6985e9ae6dd29d7e557c535a457331453898f127
Sri <srisreed@gmail.com>
2012-03-22 15:03:46 -0700

    I commented out everything I am currently not focusing on...

--- src/lmp/LMP2.java
+++ src/lmp/LMP2.java
@@ -10,111 +10,60 @@ import java.util.Random;
  * @author T-CAPS
  */
 public class LMP2 {
-    private static final int NUM_OF_NODES = 20; // the # of stations to transmit between
-    private static final int TIMES_TO_RUN = 100; // how times to simulate transmission between the # of stations
-    private static final int MAX_LAMBDA = 20;
+    private static final int NUM_OF_NODES = 5; // the # of stations to transmit between
+    //private static final int TIMES_TO_RUN = 100; // how times to simulate transmission between the # of stations
+    //private static final int MAX_LAMBDA = 20;
     //private static Random rand;
-    /**
-     * @param args the command line arguments
-     */
-   
-    public static ArrayList simulate(int nodes)
-    { 
-        return null; //don't know what to put for now
-    }
     
-    /**
-     * This gives our random variable
-     * @param lambda the lambda value
-     * @return random variable
-     */
-    public static double Poisson(double lambda)
+    public static double simulate()
     {
-    	double u = 0;
-    	double val;
-    	Random rand;
-    	rand = new Random();
-    	double max = Double.MAX_VALUE;
-    	double min = Double.MIN_VALUE;
+    	ArrayList<Node2> network = new ArrayList();
+        Node2 node;
     	
-    	while (u == 0)
-    	{
-           
-    	  //u = Math.random()/Double.MAX_VALUE;;
-          //u = rand.nextInt();
-    		u = (min + (Math.random() * ((max - min) + 1))) / Double.MAX_VALUE;
-          //System.out.println("val IS: " + u);
-    	}
-    	val = - Math.log(u)*lambda; // x= -λ log u
-        //System.out.println("val IS: " + val);
-    	
-    	
-    	return val;
-    }
-    
-    public static double simulate(double lambda)
-    {
-    	ArrayList<Node> network = new ArrayList<Node>();
-    	
-        double current = 0;
-        double next = 0;
-        double prev = -1;
-        current = Poisson(lambda);
-        next = current + Poisson(lambda);
+        //double current;
+        //double next;
+        //double prev = -1;
+        //current = node.Poisson();
+        //next = current + node.Poisson();
         
-    	Node node = new Node();
 
         for (int i = 0; i < NUM_OF_NODES; i++)
-        {
-        	network.add(node);
+        {  
+               node = new Node2(0);         // So basically what I'm trying to do here
+               network.add(node);           // is to add new node, intialize it's time to 0
+               network.set(i, node.send()); // then set EACH node to a randomized X value...clearly set WONT work 
+                                            // Some help/new direction would be appreciated! 
+               System.out.println("# " + network.get(i).toString());
         }
         
-        // while collision, ajacent times within +/- 1 slot
-        while( (current - prev < 1) || (next - current < 1) )
-        {
-            prev = current;
-            current = next;
-            next += Poisson(lambda);
-            //return true;
-        }
-        return current;
-        //return false;
+        // while collision, adjacent times within +/- 1 slot
+//        while( (current - prev < 1) || (next - current < 1) )
+//        {
+//            prev = current;
+//            current = next;
+//            next += Poisson(lambda);
+//        }
+//        
+//        return current;
+        return 0;
     }
-    
-    /*
-    public static double simulate(double lambda)
-    {
-        double current = 0;
-        double next = 0;
-        double prev = -1;
-        current = Poisson(lambda);
-        next = current + Poisson(lambda);
-        
-        // while collision, ajacent times within +/- 1 slot
-        while( (current - prev < 1) || (next - current < 1) )
-        {
-            prev = current;
-            current = next;
-            next += Poisson(lambda);
-            //return true;
-        }
-        return current;
-        //return false;
-    }*/
+
     
     public static void main(String[] args) {
-    	int i;
-        double sum;
-        double lambda;
-        for(lambda = 2.0; lambda <= 20.00; lambda += 2.0)
-        {
-            sum = 0;
-            for(i = 0; i < TIMES_TO_RUN; i++)
-            {
-                sum += simulate(lambda);
-                System.out.println("Lambda\t" +  lambda + "\tsum\t" +  sum);
-            }
-        }
+//    	int i;
+//        double sum;
+//        double lambda;
+//        for(lambda = 2.0; lambda <= 20.00; lambda += 2.0)
+////        {
+////            sum = 0;
+////            for(i = 0; i < TIMES_TO_RUN; i++)
+////            {
+////                //sum += simulate(lambda);
+////                //System.out.println("Lambda\t" +  lambda + "\tsum\t" +  sum);
+////            }
+////        }
+            
+           simulate();
         
         
         
--- src/lmp/Node2.java
+++ src/lmp/Node2.java
@@ -1,19 +1,21 @@
 package lmp;
 
+import java.util.Random;
+
 /**
  *
  * @author firen
  */
 public class Node2 {
     private static double LAMBDA = 20.0; //added temporarily to test
-    private static double t; //TIME TO SEND
+    private static double time; //TIME TO SEND
     
     /**
      * Initializes a new Node at time t
      */
-    public Node2()
+    public Node2(double aTime)
     {
-        t = 0;
+        time = aTime;
     }
 
     
@@ -36,8 +38,40 @@ public class Node2 {
     /**
      * Adds Poisson X value to current time
      */
-    public void send()
+    public double send()
     {
-        t += Poisson();
+        time += Poisson();
+        return time;
     }
+    
+   
+    /**
+     * This gives our random variable
+     * @param lambda the lambda value
+     * @return random variable
+     */
+   /* public static double Poisson(double lambda)
+    {
+    	double u = 0;
+    	double val;
+    	Random rand;
+    	rand = new Random();
+    	double max = Double.MAX_VALUE;
+    	double min = Double.MIN_VALUE;
+    	
+    	while (u == 0)
+    	{
+           
+    	  //u = Math.random()/Double.MAX_VALUE;;
+          //u = rand.nextInt();
+    		u = (min + (Math.random() * ((max - min) + 1))) / Double.MAX_VALUE;
+          //System.out.println("val IS: " + u);
+    	}
+    	val = - Math.log(u)*lambda; // x= -λ log u
+        //System.out.println("val IS: " + val);
+    	
+    	
+    	return val;
+    }
+    */
 }
