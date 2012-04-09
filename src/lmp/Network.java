bc3a71a81affa38663e35a59fdf352727f8eae01
kamron <adrianmartinezp@gmail.com>
2012-04-08 21:35:54 -0700

    General restructuring of Network.java TODO: Fix the if-else and get rid of the xVal ArrayList

--- src/lmp/Network.java
+++ src/lmp/Network.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public class Network 
 {
-    private static ArrayList<Double> xVal = new ArrayList();
-    private static Node2 node;
+    private static ArrayList<Double> xVal = new ArrayList(); // This needs to go eventually
+    private static ArrayList<Node2> nodes = new ArrayList(); // Will eventually store nodes
+    // private static Node2 node;
     private static int numOfStations = 20;
     private static int RUNCOUNT = 100;
     private static Double min = 0.0;
@@ -16,7 +17,10 @@ public class Network
     
     public static Double simulate(double lambda) 
     {
-        
+        for(int i = 0; i < numOfStations; i++)
+        {
+           nodes.add(new Node2(lambda)); //Add nodes into the ArrayList
+        }
         int indexOfMin = 0;
         double current = 0.0;
         int collisionCount = 0;
@@ -24,11 +28,11 @@ public class Network
         numOfSuccessfulPackets = 0;
         timeSuccessful = 0;
         
-        node = new Node2(lambda);
+       // node = new Node2(lambda); // create a single node
         
         for (int i = 0; i < numOfStations; i++) 
         {
-            current = node.poisson(lambda);
+            current = nodes.get(i).poisson(lambda); // call poisson with a specified lambda
             xVal.add(i, current);
         }
 
@@ -43,7 +47,8 @@ public class Network
             {
                 if (xVal.get(j) - min <= 1 && j != indexOfMin) 
                 {
-                    noCollision = false;
+                    // System.out.println("Collision!");
+                    noCollision = false; // A collision!
                 }
             }
             
@@ -53,13 +58,13 @@ public class Network
                 timeSuccessful += 8;
                 return min;
             } 
-            else 
+            else // There was a collision
             {
-                
-                xVal.set(indexOfMin, min + node.poisson(lambda));
+                // FIX THIS LINE:
+                // xVal.set(indexOfMin, min + node.poisson(lambda)); // TODO: change to Exp Backoff for Part D
                 
                 collisionCount++;
-                //xVal.set(indexOfMin, min + node.backoff(collisionCount));
+                //xVal.set(indexOfMin, min + node.backoff(collisionCount)); // Should be collisions of a particular node, not total collisions
                 indexOfMin = getMin();
                 min = xVal.get(indexOfMin); //updated to new min
             }
@@ -104,6 +109,9 @@ public class Network
          * delay = (timeSentSuccessfully - timeCreated);
          * delay jitter (variance?)
          */
+        
+        System.out.println(args.length); // used for invoking via terminal
+        
         Double sum = 0.0;
         Double lambda = 0.0;
         double throughput = 0.0;
