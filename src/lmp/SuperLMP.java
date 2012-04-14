877b14d8825ba60c895ad53c181a51bd805694bb
kamron <adrian@adrianblog.net>
2012-04-14 00:03:33 -0700

    Added double CONVERSION to go from bytes to time slots.

--- src/lmp/SuperLMP.java
+++ src/lmp/SuperLMP.java
@@ -10,14 +10,15 @@ package lmp;
 public class SuperLMP {
     public static Double result;
     public static final int SLOT_LIMIT=20000; // Run until this many time slots have passed
-    public static final int AVG_PACKET_SIZE = 8; // Average packet size
+    public static final int AVG_PACKET_SIZE = 512; // Average packet size
+    public static final double CONVERSION = 0.015625; // The conversion factor to go from bytes -> time slots
     public static void main(String[] args) {
         int throughAverage=0;
         for(int i=0;i<=50;i++)
         {
         Simulator mario = new Simulator(20, 20.0);
         double sum = 0;
-        while(mario.getPacketsSent() * AVG_PACKET_SIZE <= SLOT_LIMIT)
+        while((mario.getPacketsSent() * AVG_PACKET_SIZE * CONVERSION) <= SLOT_LIMIT)
         {
         result = mario.send();
         if(result==-1)
@@ -31,7 +32,7 @@ public class SuperLMP {
         System.out.println("Packets sent successfully: "+mario.getSuccessfulPacketsSent());
         }
         System.out.println("Calculated throughput is: " + (mario.getSuccessfulPacketsSent() * AVG_PACKET_SIZE * 512) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6))); //Throughput!
-        throughAverage+=(mario.getSuccessfulPacketsSent() * AVG_PACKET_SIZE * 512) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6));
+        throughAverage+=(mario.getSuccessfulPacketsSent() * 8 * AVG_PACKET_SIZE ) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6));
         }
         System.out.println("Average throughput is: " + throughAverage/50);
 }
