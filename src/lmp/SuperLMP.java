41415dc0f2c2b091f942ededf90992f5a50d38a5
kamron <adrian@adrianblog.net>
2012-04-14 16:09:19 -0700

    Now allows for one to choose the number of times to run. Also fixed an off-by-one error in the average throughput.

--- src/lmp/SuperLMP.java
+++ src/lmp/SuperLMP.java
@@ -20,6 +20,7 @@ public class SuperLMP extends JPanel {
     public static int NUM_OF_STATIONS = 20;
     public static double MICROSECONDS = 51.2;
     public static double lambda = 20.0;
+    public static int TIMES_TO_RUN = 1; // The times to run. If more than one, it will average the values
 
     public static void main(String[] args) {
         String s1 = JOptionPane.showInputDialog(null, "Enter the number of stations");
@@ -28,10 +29,12 @@ public class SuperLMP extends JPanel {
         AVG_PACKET_SIZE = Integer.parseInt(s2);
         String s3 = JOptionPane.showInputDialog(null, "Enter lambda");
         lambda = Double.parseDouble(s3);
+        String s4 = JOptionPane.showInputDialog(null, "How many times would you like to run this?");
+        TIMES_TO_RUN = Integer.parseInt(s4);
         JFrame frame = new JFrame();
         JPanel boardPanel = new JPanel();
         int throughAverage = 0; // Used for calculating average throughput
-        for (int i = 0; i <= 50; i++) {
+        for (int i = 1; i <= TIMES_TO_RUN; i++) { // Start at index i=1 to avoid an off-by-one error
             Simulator mario = new Simulator(NUM_OF_STATIONS, lambda);
             double sum = 0; // The summation of sums!
             while ((mario.getPacketsSent() * AVG_PACKET_SIZE * CONVERSION) <= SLOT_LIMIT) {
@@ -46,12 +49,12 @@ public class SuperLMP extends JPanel {
                 System.out.println("Packets sent successfully at time " + mario.getSuccessfulPacketsSent());
             }            
             // Print out the calculated throughput
-            System.out.println("Calculated throughput is: " + (mario.getSuccessfulPacketsSent() * AVG_PACKET_SIZE * 512) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6))); //Throughput!
+            System.out.println("Calculated throughput is: " + (mario.getSuccessfulPacketsSent() * AVG_PACKET_SIZE * 8) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6))); //Throughput!
             // Calculate the average throuput
             throughAverage += (mario.getSuccessfulPacketsSent() * 8 * AVG_PACKET_SIZE) / (SLOT_LIMIT * 51.2 * Math.pow(10, -6));
         }
         // I think this is obvious
-        System.out.println("Average throughput is: " + throughAverage / 50);
+        System.out.println("Average throughput is: " + (throughAverage / TIMES_TO_RUN));
         // So is this
         System.out.println("Traffic load is: " + (NUM_OF_STATIONS * AVG_PACKET_SIZE * 8)/(lambda * MICROSECONDS));
     }
