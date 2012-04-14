1d9f782d39d6d7be05cf0b15e0a36bfafc7e34f0
kamron <adrian@adrianblog.net>
2012-04-13 18:47:16 -0700

    Crude support for running until SLOT_LIMIT by multiplying successful packets sent by AVG_PACKET_SIZE. This needs to be fixed in Simulator.java and possibly in Node2.java to support time slots instead.

--- src/lmp/SuperLMP.java
+++ src/lmp/SuperLMP.java
@@ -9,8 +9,13 @@ package lmp;
  */
 public class SuperLMP {
     public static Double result;
+    public final int SLOT_LIMIT=20000; // Run until this many time slots have passed
+    public static final int AVG_PACKET_SIZE = 8; // Average packet size
     public static void main(String[] args) {
         Simulator mario = new Simulator(20, 20.0);
+        double sum = 0;
+        while(mario.getSuccessfulPacketsSent() * AVG_PACKET_SIZE <=20000)
+        {
         result = mario.send();
         if(result==-1)
         {
@@ -18,5 +23,9 @@ public class SuperLMP {
         }
         else
             System.out.println("Hooray! You sent at time: "+result);
-    }
+        
+        sum=+mario.getSentTime(); // Add the sent times together
+        System.out.println("Packets sent successfully: "+mario.getSuccessfulPacketsSent());
+        }
+}
 }
