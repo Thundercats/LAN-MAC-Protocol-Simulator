2b0cf749da16f830f7c90bad64a8ccdc6dff7365
kamron <adrian@adrianblog.net>
2012-04-13 17:55:55 -0700

    I *think* I got Simulator.java working with backoff() and poisson()

--- src/lmp/Simulator.java
+++ src/lmp/Simulator.java
@@ -5,6 +5,8 @@
 package lmp;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.ListIterator;
 
 /**
@@ -14,12 +16,16 @@ import java.util.ListIterator;
 public class Simulator {
     private ArrayList<Node2> fred;
     private Double lambda;
+    private ArrayList collision;
     Simulator(int numOfStations, Double lambda)
     {
         fred = new ArrayList(); // Hello fred!
+        collision = new ArrayList(); // Keeps track of collisions
         this.lambda = lambda;
         for(int i=1;i<=numOfStations;i++)
-            fred.add(new Node2(lambda)); // Create new Nodes!
+            fred.add(new Node2(lambda)); // Create new Nodes initilzed with time poisson()
+        // Sort fred
+        Collections.sort(fred);
     }
     
     /**
@@ -28,7 +34,6 @@ public class Simulator {
      */
     public Double send()
     {
-        ArrayList<Node2> collision = new ArrayList();
         for(int i=0;i<fred.size();i++)
         {
             fred.get(i).send(lambda); // Send!
@@ -37,13 +42,31 @@ public class Simulator {
                 fred.get(j).send(lambda); // Send!
                 if(Math.abs(fred.get(i).getTime()-fred.get(j).getTime())<=1) 
                 {
-                    System.out.println("Collision!"); // Oh noes!
+                    // System.out.println("Collision!"); // Oh noes!
+                    fred.get(j).collide();
+                    fred.get(i).collide();
                     collision.add(fred.get(j)); // Add to a running list of collisions
+                    collision.add(fred.get(i)); // Add to a running list of collisions
+                    
                 }
             }
             if(collision.isEmpty())
                 return fred.get(i).getTime();
         }
+        collision.clear();
         return -1.0;
+    }
+    
+    public double tryAgain()
+    {
+        // Add collisions back into the running nodes
+        // fred.addAll(collision);
+        Collections.sort(fred);
+        double result=-1;
+        while(result==-1)
+        {
+            result = send();
+        }
+        return result;
     }
 }
